4ef89b99716b954391a112a797650f3961e899f4
zlurker <tallspin_97@hotmail.com>
2020-03-23 21:52:09 +0800

    Initial commit

---
+++ Source/DTD/DTDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DTDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DTD_API ADTDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
