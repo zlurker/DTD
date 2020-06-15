87a43b636c1664d3745086fe4be005cdaeb7cbfc
zlurker <tallspin_97@hotmail.com>
2020-06-15 16:05:26 +0800

    Tower Radius Creep Detection
    After lots of experimenting and R&D I have created the detection system within the Tower Radius. Interestingly, if we build a tower with the creep in range, it wouldn't be able to register it in its range. Hence, I added another functionality at the start to make sure that it does a pre-scan once and aqquires all target. Everything after that is then registered and deregistered with the detection system.

--- Source/DTD/Private/Tower.cpp
+++ Source/DTD/Private/Tower.cpp
@@ -6,20 +6,36 @@
 // Sets default values
 ATower::ATower()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Sets up attack radius for later use.
 	attackRadius = CreateDefaultSubobject<USphereComponent>(TEXT("Tower Attack Radius"));
 	attackRadius->SetupAttachment(RootComponent);
 	attackRadius->bHiddenInGame = false;
-	
+
 }
 
 // Called when the game starts or when spawned
 void ATower::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
 	attackRadius->SetSphereRadius(towerAttackRadius);
+	attackRadius->OnComponentBeginOverlap.AddDynamic(this, &ATower::OnOverlapBegin);
+	attackRadius->OnComponentEndOverlap.AddDynamic(this, &ATower::OnOverlapEnd);
+
+
+	TArray<TEnumAsByte<EObjectTypeQuery>> traceObjectTypes;
+	TArray<AActor*> ignoreActors;
+
+	UClass* seekClass = ACreepBase::StaticClass();
+	
+
+	UKismetSystemLibrary::SphereOverlapActors(GetWorld(), GetActorLocation(), towerAttackRadius, traceObjectTypes, seekClass, ignoreActors, aqquiredTargets);
+
+	UE_LOG(LogTemp, Log, TEXT("Tower initialised... Current creep aqquired: %d"), aqquiredTargets.Num());
+	//for (AActor* overlappedActor : aqquiredTargets)
+		//UE_LOG(LogTemp, Log, TEXT("OverlappedActor: %s"), *overlappedActor->GetName());
 }
 
 // Called every frame
@@ -33,9 +49,31 @@ void ATower::Tick(float DeltaTime)
 		prevAction = currTime;
 		TowerAction();
 	}
+}
 
+void ATower::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	ACreepBase* creep = Cast<ACreepBase>(OtherActor);
 
+	UE_LOG(LogTemp, Log, TEXT("Something came in contact."));
 
+	if (creep != nullptr) {
+		aqquiredTargets.Add(OtherActor);
+		UE_LOG(LogTemp, Log, TEXT("Creep is in range. Adding to array... Current creep aqquired: %d"), aqquiredTargets.Num());	
+	}
+
+}
+
+void ATower::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	ACreepBase* creep = Cast<ACreepBase>(OtherActor);
+
+	UE_LOG(LogTemp, Log, TEXT("Something has left range of tower."));
+
+	if (creep != nullptr) {
+		aqquiredTargets.Remove(OtherActor);
+		UE_LOG(LogTemp, Log, TEXT("Creep is out of range. Removing from array... Current creep aqquired: %d"), aqquiredTargets.Num());
+	}
 }
 
 void ATower::TowerAction() {
--- Source/DTD/Public/Tower.h
+++ Source/DTD/Public/Tower.h
@@ -8,6 +8,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "CreepBase.h"
 #include "Tower.generated.h"
 
 UCLASS()
@@ -42,9 +43,15 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Tower Behaviour")
 		bool enableTargetting;
 
-	
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp,class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** called when something leaves the sphere component */
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp,class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 private:
 	// Internal variables
 	float prevAction;
 	USphereComponent* attackRadius;
+	TArray<AActor*> aqquiredTargets;
 };
